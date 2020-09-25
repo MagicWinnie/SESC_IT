8603595faa1a77753e045ed87215e5bd89435311
MagicWinnie <magicwinnie@yandex.ru>
2020-09-25 22:40:26 +0700

    more tasks

--- IT_Lessons/22.c
+++ IT_Lessons/22.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <math.h>
+
+int summ(a, n)
+{
+    int a1 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        a1 += a % 10;
+        a = a / 10;
+    }
+
+    return a1;
+}
 
 int main()
 {
-    int count = 0;
-
-    for (int i = 0; i < 1000000; i++)
+    int n, count = 0;
+    scanf("%d", &n);
+    for (int i = 0; i < pow(10, n); i++)
     {
-        if (i/100000 + i/10000%10 + i/1000%10 == i%1000/100 + i%1000/10%10 + i%1000%10)
+        if (summ(i/(int)pow(10, n/2), n/2)==summ(i%(int)pow(10, n/2), n/2))
             count++;
     }
-
     printf("%d", count);
-
     printf("\n");
         
 }
