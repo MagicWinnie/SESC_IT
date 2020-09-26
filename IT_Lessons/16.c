a2141fffe8933b5b9e80e5bbff2169b38397698a
MagicWinnie <magicwinnie@yandex.ru>
2020-09-26 18:02:45 +0700

    fixed

--- IT_Lessons/16.c
+++ IT_Lessons/16.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 
-int main()
+struct dateStruct 
+{ 
+    int d, m, y; 
+}; 
+  
+int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; 
+
+int leapYears(struct dateStruct d) 
+{ 
+    int years = d.y; 
+   
+    if (d.m <= 2)
+        years--; 
+  
+    return years / 4 - years / 100 + years / 400; 
+} 
+  
+int getDifference(struct dateStruct ds1, struct dateStruct ds2) 
+{ 
+    long int n1 = ds1.y*365 + ds1.d; 
+  
+    for (int i=0; i<ds1.m - 1; i++) 
+        n1 += monthDays[i]; 
+  
+    n1 += leapYears(ds1); 
+  
+    long int n2 = ds2.y*365 + ds2.d; 
+    for (int i=0; i<ds2.m - 1; i++) 
+        n2 += monthDays[i]; 
+    n2 += leapYears(ds2); 
+  
+    return (n2 - n1); 
+} 
+  
+int main() 
 {
     int d1, m1, y1, d2, m2, y2;
     scanf("%d %d %d %d %d %d", &d1, &m1, &y1, &d2, &m2, &y2);
-    
-    int ak1, ak2, yk1, yk2, mk1, mk2;
 
-    ak1 = (14-m1)/12;
-    ak2 = (14-m2)/12;
+    struct dateStruct ds1 = {d1, m1, y1}; 
+    struct dateStruct ds2 = {d2, m2, y2};
+    printf("%d\n", getDifference(ds1, ds2)); 
 
-    yk1 = y1 + 4800 - ak1;
-    yk2 = y2 + 4800 - ak2;
-
-    mk1 = m1 + 12*ak1 - 3;
-    mk2 = m2 + 12*ak2 - 3;
-
-    int jdn1, jdn2;
-
-    jdn1 = d1 + (153*m1 + 2)/5 + 365*y1 + y1/4 - y1/100 + y1/400 - 32045;
-    jdn2 = d2 + (153*m2 + 2)/5 + 365*y2 + y2/4 - y2/100 + y2/400 - 32045;
-
-    printf("%d", jdn2-jdn1);
-    
-    printf("\n");
-        
-}
+    return 0; 
+} 
