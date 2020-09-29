69a4c3e16423e11a89711e42c2fb83c49252de6f
MagicWinnie <magicwinnie@yandex.ru>
2020-09-29 14:01:20 +0700

    39

--- IT_Lessons/39.c
+++ IT_Lessons/39.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <math.h>
+
+// double power(double x, double y) 
+// { 
+//     return exp(y*log(x));
+// } 
+
+double power(double x, int y)  
+{  
+    if (y == 0)  
+        return 1;
+
+    double temp = power(x, y / 2);  
+
+    if (y%2 == 0)  
+        return temp*temp;  
+    if(y > 0)  
+        return x*temp*temp;  
+    return (temp*temp)/x;  
+}  
+  
 
 int main()
 {
-    double n;
-    scanf("%lf", &n);
-    printf("%lf", power());
+    double a, b;
+    scanf("%lf %lf", &a, &b);
+    printf("%lf\n", power(a, b));
 }
