eef58a3a20f9daefaa6a8e6e518ddcff206b0f32
MagicWinnie <magicwinnie@yandex.ru>
2020-09-29 13:41:24 +0700

    added

---
+++ IT_Lessons/38.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned long long int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    unsigned long long int res = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        res *= i;
+    }
+    return res; 
+} 
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", factorial(n));
+}
