cb15cc5813e34c78f16bcf48e1d04f02a97e8ffd
MagicWinnie <magicwinnie@yandex.ru>
2020-09-29 17:29:38 +0700

    36

---
+++ IT_Lessons/36.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int conjunction(int a, int b)
+{
+    return a&b;
+}
+
+int disjunction(int a, int b)
+{
+    return a|b;
+}
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d %d\n", conjunction(a, b), disjunction(a, b));
+}
