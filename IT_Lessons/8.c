a4f3fd4230c6e06e56242f51b7a08f7ba53d1e3f
Dmitrii Okoneshnikov <39788810+MagicWinnie@users.noreply.github.com>
2020-10-02 12:56:01 +0700

    fixed bug

--- IT_Lessons/8.c
+++ IT_Lessons/8.c
@@ -5,9 +5,9 @@ int main()
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     
-    if (a > b && a > c)
+    if (a >= b && a >= c)
         printf("%d", a); 
-    else if (b > a && b > c)
+    else if (b >= a && b >= c)
         printf("%d", b);
     else
         printf("%d", c);
