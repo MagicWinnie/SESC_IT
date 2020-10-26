1c0594497b11c96a21e349fed2b554c611625734
MagicWinnie <magicwinnie@yandex.ru>
2020-10-26 22:55:09 +0700

    updated

--- IT_Lessons/51.c
+++ IT_Lessons/51.c
@@ -15,80 +15,32 @@ void printArr(int *arr, int size)
 
 int main()
 {
-    int size = 10, temp = 0, maxV = -15, maxV2 = 15, maxI = 0, maxI2 = 0;
-    int arr1[size], arr2[size], arr3[size*2];
+    int size1, size2, temp = 0, maxV = -15, maxV2 = 15, maxI = 0, maxI2 = 0;
+    scanf("%d %d", &size1, &size2);
+    int arr1[size1], arr2[size2], arr3[size1+size2];
 
     // filling array
     srand(time(NULL));
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < size1; i++)
     {
         arr1[i] = (rand() % (10 - 1 + 1)) + 1;
+    }
+
+    for (int i = 0; i < size2; i++)
+    {
         arr2[i] = (rand() % (10 - 1 + 1)) + 1;
     }
 
-    for (int i = 0; i < size*2; i++)
+    for (int i = 0; i < size1+size2; i++)
     {
         arr3[i] = 0;
     }
 
-    printArr(arr1, size);
-    printArr(arr2, size);
+    printArr(arr1, size1);
+    printArr(arr2, size2);
 
-    int a = size;
+    
 
-    while (a>1){
-        for (int i = 0; i < a; i++)
-        {
-            if (arr1[i] > maxV){
-                maxV = arr1[i];
-                maxI = i;
-            }
-            if (arr2[i] > maxV2){
-                maxV2 = arr2[i];
-                maxI2 = i;
-            }
-        }
-
-        temp = arr1[a-1];
-        arr1[a-1] = maxV;
-        arr1[maxI] = temp;
-
-        temp = arr2[a-1];
-        arr2[a-1] = maxV2;
-        arr2[maxI2] = temp;
-
-        a--;
-
-        for (int i = size*2-1; i >= 0; i--)
-        {
-            if (arr3[i] > maxV && arr3[i] > maxV2)
-            {
-                continue;
-            } else if (maxV >= maxV2) {
-                temp = arr3[i];
-                arr3[i] = maxV;
-                arr3[i-2] = temp;
-
-                temp = arr3[i-1];
-                arr3[i-1] = maxV2;
-                arr3[i-3] = temp;
-                break;
-            } else {
-                temp = arr3[i];
-                arr3[i] = maxV2;
-                arr3[i-2] = temp;
-                
-                temp = arr3[i-1];
-                arr3[i-1] = maxV;
-                arr3[i-3] = temp;
-                break;
-            }
-        }
-
-        maxV = -15;
-        maxV2 = -15;
-    }
-
-    printArr(arr3, size*2);
+    printArr(arr3, size1+size2);
 }
