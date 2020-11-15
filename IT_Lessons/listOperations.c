19e58453bbb26c33a8b9b0518d90a3fdf1c249bc
MagicWinnie <magicwinnie@yandex.ru>
2020-11-15 16:36:32 +0700

    Added list oper and struct arr sort

---
+++ IT_Lessons/listOperations.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list
+{
+    int val;
+    struct list *next;
+};
+
+void print_list(struct list *head)
+{
+    while (head != NULL)
+    {
+        printf("%d ", head->val);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void deleteAll(struct list *head)
+{
+    struct list *curr = head;
+    while (head != NULL)
+    {
+        head = head->next;
+        free(curr);
+        curr = head;
+    }
+}
+
+struct list *add2front(struct list *head, int val)
+{
+    struct list *new = (struct list*)malloc(sizeof(struct list));
+    new->val = val;
+    struct list *temp = head;
+    new->next = temp;
+    return new;
+}
+
+struct list *add2end(struct list *head, int val)
+{
+    if (head == NULL)
+    {
+        head = (struct list*)malloc(sizeof(struct list));
+        head->val = val;
+        head->next = NULL;
+        return head;
+    }
+
+    struct list *temp = head;
+    struct list *new = (struct list*)malloc(sizeof(struct list));
+    
+    new->val = val;
+    new->next = NULL;
+    while (temp->next != NULL)
+        temp = temp->next;
+    temp->next = new;
+    return head;
+}
+
+struct list *addVal(struct list *head, int num, int ind)
+{
+    struct list *temp, *left, *right;
+    right = head;
+    for (int i = 0; i < ind; i++)
+    {
+        left=right;
+        right=right->next;
+    }
+    temp = (struct list*)malloc(sizeof(struct list));
+    temp->val = num;
+    left->next = temp;
+    left = temp;
+    left->next = right;
+    return head;
+}
+
+struct list *deleteVal(struct list *head, int val)
+{
+    struct list *temp = head;
+    if (temp->val == val)
+    {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+    while (temp->next != NULL && temp->next->val != val)
+        temp = temp->next; 
+    if (temp->next == NULL) return head;
+    struct list *temp_next = temp->next->next;
+    free(temp->next);
+    temp->next = temp_next;
+    return head;
+}
+
+int main()
+{
+    struct list *head = NULL;
+    head = add2end(head, 10);
+    head = add2front(head, 2);
+    head = add2front(head, 1);
+    print_list(head);
+    head = add2end(head, 3);
+    head = add2end(head, 4);
+    head = deleteVal(head, 2);
+    print_list(head);
+    head = addVal(head, 12, 2);
+    print_list(head);
+    deleteAll(head);
+    return 0;
+}
---
+++ IT_Lessons/structSort.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 10    
+
+struct Student 
+{
+    int id;
+    char *name;
+    float mathAverage;
+    float physAverage;
+};
+
+void printStudents(struct Student Students[])
+{
+    printf("ID\tName\t\tAvg.Maths\tAvg.Physics\n");
+    for (int i = 0; i < N; i++)
+        printf("%d\t%s\t\t%f\t%f\n", Students[i].id, Students[i].name, Students[i].mathAverage, Students[i].physAverage);
+    printf("\n");
+}
+
+float float_rand()
+{
+    float scale = rand()/(float) RAND_MAX;
+    return 1 + scale*5;
+}
+
+void sortStudents(struct Student Students[], int type)
+{
+    struct Student temp;
+    
+    for (int i = 0; i < N-1; i++)
+    {
+        for (int j = 0; j < (N-i-1); j++)
+        {
+            switch (type)
+            {
+            case 1:
+                if (Students[j].id > Students[j+1].id)
+                {
+                    temp = Students[j];
+                    Students[j] = Students[j+1];
+                    Students[j+1] = temp;
+                }
+                break;
+            case 2:
+                if (*Students[j].name > *Students[j+1].name)
+                {
+                    temp = Students[j];
+                    Students[j] = Students[j+1];
+                    Students[j+1] = temp;
+                }
+                break;
+            case 3:
+                if (Students[j].mathAverage > Students[j+1].mathAverage)
+                {
+                    temp = Students[j];
+                    Students[j] = Students[j+1];
+                    Students[j+1] = temp;
+                }
+                break;
+            case 4:
+                if (Students[j].physAverage > Students[j+1].physAverage)
+                {
+                    temp = Students[j];
+                    Students[j] = Students[j+1];
+                    Students[j+1] = temp;
+                }
+                break;
+            default:
+                if (Students[j].id > Students[j+1].id)
+                {
+                    temp = Students[j];
+                    Students[j] = Students[j+1];
+                    Students[j+1] = temp;
+                }   
+            } 
+        }
+    }
+}
+
+
+int main()
+{
+    srand(time(NULL));
+    char* names[N] = {"Dmitrii", "Alex", "Nikita", "Nikolay", "Alina", "Elena", "Maxim", "Egor", "Igor", "Alena"};
+    struct Student Students[N];
+    for (int i = 0; i < N; i++)
+    {
+        Students[i].id = i+1;
+        Students[i].name = names[rand()%N];
+        Students[i].mathAverage = float_rand();
+        Students[i].physAverage = float_rand();
+    }
+    sortStudents(Students, 1);
+    printStudents(Students);
+    sortStudents(Students, 2);
+    printStudents(Students);
+    sortStudents(Students, 3);
+    printStudents(Students);
+    sortStudents(Students, 4);
+    printStudents(Students);
+    return 0;
+}
