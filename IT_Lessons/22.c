b5415f40a0b25fc4eced8fc97561aa93d5ace012
MagicWinnie <magicwinnie@yandex.ru>
2020-12-06 12:16:53 +0700

    WIP

--- IT_Lessons/22.c
+++ IT_Lessons/22.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int summ(int a, int n)
+void getNext(int *arr, int n)
 {
-    int a1 = 0;
-    for (int i = 0; i < n; i++)
+    int *arrOld = (int*)malloc(n*sizeof(int));
+    for (int i = 0; i < n; i++) arrOld[i] = arr[i];
+    int new = n + 8;
+    arr = (int*)realloc(arr, new*sizeof(int));
+    for (int i = 0; i <= new; i++)
     {
-        a1 += a % 10;
-        a = a / 10;
+        int q = 0;
+        for (int j = 0; j < 10; j++)
+        {
+            if (i-j >= 0 && i-j < n) q += arrOld[i-j];
+        }
+        printf("q: %d\n", q);
+        arr[i] = q;
     }
-
-    return a1;
+    // for (int i = 0; i <= new; i++) printf("%d,", arr[i]);
+    // printf("\n");
 }
 
 int main()
 {
-    int n, count = 0;
+    int n, count = 0, size = 10;
+    int *arr = (int*)malloc(size*sizeof(int));
     scanf("%d", &n);
-    if (n%2!=0){
-        printf("неравное количество цифр\n");
+    if (n % 2 != 0){
+        printf("Not equal number of digits\n");
         return 0;
     }
-    for (int i = 0; i < pow(10, n); i++)
+    for (int i = 0; i < 10; i++) arr[i] = 1;
+    for (int i = 0; i < n/2 - 1; i++)
     {
-        if (summ(i/(int)pow(10, n/2), n/2)==summ(i%(int)pow(10, n/2), n/2))
-            count++;
+        getNext(arr, size);
+        for (int i = 0; i <= size; i++) printf("%d, ", arr[i]);
+        printf("\n");
+        size += 9;
     }
-    printf("%d", count);
-    printf("\n");
-        
+    for (int i = 0; i < size; i++)
+    {
+        count += (int)pow(arr[i], 2);
+        printf("%d ", arr[i]);
+    }
+    printf("%d\n", count);        
 }
