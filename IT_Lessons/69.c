23a7457d2525636461bb56e767a1749553596686
MagicWinnie <magicwinnie@yandex.ru>
2020-12-10 21:25:56 +0700

    69 now works

--- IT_Lessons/69.c
+++ IT_Lessons/69.c
@@ -1,62 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void reverse_array(int a[], int n)
+struct list
 {
-    int c, t;
+    int val;
+    struct list *next;
+};
 
-    for (c = 0; c < n / 2; c++) {
-        t = a[c];
-        a[c] = a[n - c - 1];
-        a[n - c - 1] = t;
+void print_list(struct list *head)
+{
+    while (head != NULL)
+    {
+        printf("%d ", head->val);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+void deleteAll(struct list *head)
+{
+    struct list *curr = head;
+    while (head != NULL)
+    {
+        head = head->next;
+        free(curr);
+        curr = head;
     }
 }
 
-void DFS(int **arr, int *path, int v, int size, int n)
+struct list *add2front(struct list *head, int val)
+{
+    struct list *new = (struct list*)malloc(sizeof(struct list));
+    new->val = val;
+    struct list *temp = head;
+    new->next = temp;
+    return new;
+}
+
+struct list *path = NULL;
+
+void DFS(int **arr, int v, int n)
 {
     arr[v][v] = 1;
 	for (int i = 0; i < n; i++)
-    {
-		int to = arr[v][i];
-		if (!arr[to][to])
-			DFS(arr, path, to, size, n);
-	}
-    path[size] = v;
-}
- 
-void topological_sort(int **arr, int n)
-{
-    int *path = (int*)calloc(1, sizeof(int));
-    int size = 1;
-
-	for (int i = 0; i < n; i++)
-		if (!arr[i][i])
-        {
-			DFS(arr, path, i, size, n);
-            size++;
-            path = (int*)realloc(path, size * sizeof(int));
-        }
-    
-	reverse_array(path, size);
-    for (int i = 0; i < size; i++) printf("%d ", path[i]);
-    printf("\n");
+		if (arr[v][i] == 1 && !arr[i][i])
+			DFS(arr, i, n);
+    path = add2front(path, v);
 }
 
 int main()
 {
     int n;
     scanf("%d", &n);
+    
     int **arr = (int**)calloc(n, sizeof(int*));
-    for (int i = 0; i < n; i++) arr[i] = (int*)calloc(n, sizeof(int));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            scanf("%d", &arr[i][j]);
-        }
-    }
 
-    topological_sort(arr, n);
+    for (int i = 0; i < n; i++) arr[i] = (int*)calloc(n, sizeof(int));
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &arr[i][j]);
+
+	for (int i = 0; i < n; i++)
+		if (!arr[i][i])
+			DFS(arr, i, n);
+
+    print_list(path);
+
+    deleteAll(path);
 
     return 0;
 }
@@ -64,9 +75,9 @@ int main()
 /*
 6
 0 1 1 0 0 0 
-1 0 1 1 0 1 
-1 1 0 0 1 0 
-0 1 0 0 1 0 
-0 0 1 1 0 1 
-0 1 0 0 1 0 
+1 0 0 1 0 0 
+1 0 0 1 0 0 
+0 1 1 0 0 0 
+0 0 0 0 0 1 
+0 0 0 0 1 0 
 */
