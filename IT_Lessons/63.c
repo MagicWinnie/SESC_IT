e40636fcf7f85aab3b0be0a07b4797bb1a4fd384
MagicWinnie <magicwinnie@yandex.ru>
2020-12-11 14:07:09 +0700

    Added 63

--- IT_Lessons/63.c
+++ IT_Lessons/63.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 int main()
 {
     int *a = (int*)malloc(sizeof(int));
-    // *a = 123456789;
     char *b = a;
-    for (int i = 0; i < 10; i++)
+    // *(b + 0) = 21;
+    // *(b + 1) = 205;
+    // *(b + 2) = 91;
+    // *(b + 3) = 7;
+    int last = 123456789;
+    for (int i = 0; i < 4; i++)
     {
-        b[i] = (char*)(i + 1);
-    }
-    printf("%s\n", *b);
+        *(b + i) = last % 256;
+        last /= 256;
+    }   
+    printf("%d\n", *a);
     return 0;
 }
+// 123456789 -   7 * 2^24 = 6016277
+// 6016277   -  91 * 2^16 = 52501
+// 52501     - 205 * 2^8  = 21
