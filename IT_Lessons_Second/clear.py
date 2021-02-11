896948000ca4480e8750f0f0a03c0cd0dd6f1137
MagicWinnie <magicwinnie@yandex.ru>
2021-02-11 13:59:33 +0700

    renamed

--- IT_Lessons_Second/clear.py
+++
@@ -1,9 +0,0 @@
-import os
-
-files = list(filter(lambda x: os.path.isfile(x), os.listdir('.')))
-
-white_list = ['cpp', 'c', 'txt', 'dat', 'json']
-
-for f in files:
-    if f.split('.')[-1] not in white_list:
-        os.remove(f)
