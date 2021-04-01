8757c4aa5f2958c6d366da4dc9b41132ad253653
MagicWinnie <magicwinnie@yandex.ru>
2021-04-01 14:25:27 +0700

    Added binary tree

---
+++ Second_Semester/binary_tree.h
@@ -0,0 +1,95 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+template <class T>
+class BinaryTree
+{
+    private:
+        typedef struct node
+        {
+            T data;
+            struct node *left;
+            struct node *right;
+        } tree;
+        tree *TREE;
+        
+        void PrintTreeRunner(tree *t, std::string prefix, bool side)
+        {
+            if ((t->left != NULL) || (t->right != NULL))
+            {
+                std::cout << prefix;
+                
+                std::cout << (side ? "├── " : "└── ");
+
+                std::cout << t->data << std::endl;
+
+                PrintTreeRunner(t->left, prefix + (side ? "│   " : "    "), true);
+                PrintTreeRunner(t->right, prefix + (side ? "│   " : "    "), false);
+            }
+        }
+        void destroyTree(tree *t)
+        {
+            if (t != NULL)
+            {
+                destroyTree(t->left);
+                destroyTree(t->right);
+                delete t;
+            }
+        }
+        void addRunner(T x, tree *t)
+        {
+            if ((t->left == NULL) && (t->right == NULL))
+            {
+                t->data = x;
+                t->left = new tree;
+                t->right = new tree;
+                t->left->left = NULL;
+                t->left->right = NULL;
+            } else {
+                if (x < t->data)
+                {   
+                    if (t->left != NULL)
+                    {
+                        addRunner(x, t->left);
+                    } else {
+                        t->left = new tree;
+                        t->data = x;
+                        t->left->left = NULL;
+                        t->left->right = NULL;    
+                    }
+                } else {
+                    if (t->right != NULL)
+                    {
+                        addRunner(x, t->right);
+                    } else {
+                        t->right = new tree;
+                        t->data = x;
+                        t->right->left = NULL;
+                        t->right->right = NULL;
+                    }
+                }
+            }
+        }
+    public:
+        BinaryTree()
+        {
+            TREE = new tree;
+            TREE->left = NULL;
+            TREE->right = NULL;
+        }
+        ~BinaryTree()
+        {
+            destroyTree(TREE);
+        }
+        void printTree()
+        {
+            std::cout << "-----------------------------------" << std::endl;
+            PrintTreeRunner(TREE, "", false);
+            std::cout << "-----------------------------------" << std::endl;
+        }
+        void add(T x)
+        {
+            addRunner(x, TREE);
+        }
+};
---
+++ Second_Semester/bintree.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "binary_tree.h"
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    BinaryTree<string> tree;
+    tree.add("23");
+    tree.add("22");
+    tree.add("32");
+    tree.printTree();
+}
