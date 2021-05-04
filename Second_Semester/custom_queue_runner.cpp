6e20efe8cfbbb9399f0aa9baff1c2a16ef6d6378
MagicWinnie <magicwinnie@yandex.ru>
2021-05-04 11:31:53 +0700

    Queue added

---
+++ Second_Semester/custom_queue_runner.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "queue_custom.h"
+
+using namespace std;
+
+int main()
+{
+    int n, temp;
+    QueueClass<int> qc;
+
+    cout << "Enter n: " << endl;
+    cin >> n;
+
+    cout << "Enter numbers:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        qc.push(temp);
+    }
+    cout << "----------" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << qc.pop() << endl;
+    }
+}
---
+++ Second_Semester/queue_custom.h
@@ -0,0 +1,72 @@
+// Stack is a linear list in which all inclusions and exclusions are made at one end of the list.
+#pragma once
+template <class T>
+class QueueClass
+{
+private:
+    struct list
+    {
+        T data;
+        struct list *next;
+    };
+    typedef struct stack
+    {
+        struct list *top;
+        struct list *bottom;
+    } Stack;
+    Stack *S;
+
+public:
+    QueueClass()
+    {
+        S = new Stack;
+        S->top = NULL;
+        S->bottom = NULL;
+    }
+    T top()
+    {
+        if (S->top)
+            return (S->top->data);
+        else
+            throw "[IndexError] Stack is empty. Cannot get the top."; // return 0; // or raise an error, 'cause the stack is empty
+    }
+    T pop()
+    {
+        T a;
+        struct list *p;
+        p = S->top;
+        a = p->data;
+        S->top = p->next;
+        delete[] p;
+        return a;
+    }
+    void push(T a)
+    {
+        struct list *p = new struct list;
+        p->data = a;
+        p->next = NULL;
+        if (!(S->top))
+        {
+            S->top = p;
+            S->bottom = p;
+            return;
+        }
+        S->bottom->next = p;
+        S->bottom = p;
+    }
+    bool empty()
+    {
+        return (S->top == NULL);
+    }
+    ~QueueClass()
+    {
+        struct list *p;
+        while (S->top)
+        {
+            p = S->top;
+            S->top = p->next;
+            delete[] p;
+        }
+        delete[] S;
+    }
+};
