265e02160b391986a311ee3a868d68875d4dbda5
MagicWinnie <magicwinnie@yandex.ru>
2021-05-10 10:32:37 +0700

    Please rewrite code when stealing

--- VOSH/2020-2021/Second_Stage/2.cpp
+++ VOSH/2020-2021/Second_Stage/2.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <cmath>
 
 using namespace std;
 
@@ -9,7 +7,7 @@ int main()
 {
     vector<vector<int>> actor_votes;
     vector<vector<int>> car_votes;
-    
+
     int n, m, temp;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
@@ -22,7 +20,6 @@ int main()
         }
         actor_votes.push_back(temp_vec);
     }
-
     for (int i = 0; i < n; i++)
     {
         vector<int> temp_vec;
@@ -43,23 +40,18 @@ int main()
                 int temp_max_actors = -1, temp_max_cars = -1;
                 for (int k = 0; k < n; k++)
                 {
-                    if (k == i) continue;
+                    if (k == i)
+                        continue;
                     if (actor_votes[k][j] > temp_max_actors)
-                    {
                         temp_max_actors = actor_votes[k][j];
-                    }
                     if (car_votes[k][j] > temp_max_cars)
-                    {
                         temp_max_cars = car_votes[k][j];
-                    }
                 }
                 if (actor_votes[i][j] >= temp_max_actors && car_votes[i][j] >= temp_max_cars)
-                {
                     sum_of_votes++;
-                }
             }
         }
-        if ((double)sum_of_votes/(double)m >= 0.3)
+        if ((double)sum_of_votes / (double)m >= 0.3)
         {
             cout << "YES" << endl;
             cout << i + 1 << endl;
@@ -71,15 +63,13 @@ int main()
     {
         int sum_of_votes = 0;
         for (int j = 0; j < m; j++)
-        {
             sum_of_votes += actor_votes[i][j];
-        }
         if (sum_of_votes > max_val)
         {
             max_val = sum_of_votes;
             max_ind = i + 1;
         }
-    }   
+    }
     cout << "NO" << endl;
     cout << max_ind << endl;
 }
